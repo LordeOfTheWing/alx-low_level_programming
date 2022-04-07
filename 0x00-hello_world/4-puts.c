e1d6160d2cb83cf6f7dff8bb181beb94f8d2220a
Clinton Mainya <klyntonmainya@gmail.com>
2022-04-07 02:53:15 -0700

    Fix Print some output according to task given

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * use puts() to print out
+ * Return: Always a 0(Success)
+ */
 int main(void)
 {
-puts("Programming is like building a multidilingual puzzle\n");
+puts("\"Programming is like building a multidilingual puzzle\n");
 return (0);
 }
