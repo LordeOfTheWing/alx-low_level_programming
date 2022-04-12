682ecb943e7b151ebbb8c10abc85ba46c0026899
Clinton Mainya <klyntonmainya@gmail.com>
2022-04-11 23:12:29 -0700

    Check for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase
+ *
+ *@c: function parameter
+ *
+ * Return: 1 if lowercase
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
